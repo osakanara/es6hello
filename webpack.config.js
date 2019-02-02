3e79ef88d94df186efd7b141209e1a7e12b555e0
osakanara <osakanarakyotonagoyatokyo@gmail.com>
2019-02-02 11:49:00 +0900

    jsファイルのライブラリ化

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,23 @@ module.exports = {
         contentBase: path.join(__dirname, "public"),
         watchContentBase: true,
         port: 8080,
+    },
+
+    // jsファイルのライブラリ化
+    // 複数のjsファイルをimportしてるファイル
+    entry: {app: './src/index.js'},
+
+    // ブラウザからバンドルにアクセスする際、以下のようにする
+    // <script src="js/app.js"></script>
+    output: {
+        publicPath: "/js/",
+        // entryのキーがnameになる
+        filename: '[name].js',
+        // パッケージ名
+        // 以下のように使う
+        // const greeting = new com.example.Greeting();
+        library: ["com", "example"],
+        // ライブラリ化の方式：Universal Module Definition
+        libraryTarget: 'umd'
     }
 };
